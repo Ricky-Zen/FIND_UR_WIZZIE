42760fd49d16c724f7642c01b19efc3ee2c94f85
Ricky Zenati <b00629577@essec.edu>
2021-02-15 22:49:11 +0100

    manually erased the fucking t.id activity?

--- db/migrate/20210215171046_create_bookings.rb
+++ db/migrate/20210215171046_create_bookings.rb
@@ -1,7 +1,6 @@
 class CreateBookings < ActiveRecord::Migration[6.0]
   def change
     create_table :bookings do |t|
-      t.id :activity
       t.references :user, null: false, foreign_key: true
       t.integer :price
       t.date :date
