b9daec2d560d4813e7f59cb1b0b22a120a7c6c83
Ricky Zenati <b00629577@essec.edu>
2021-02-15 17:18:28 +0100

    activities controller, theme model, starting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'index', to: 'activities#index'
+  # get 'show', to: 'activities#show'
+  # post 'create', to: 'activities#create'
+  # get 'new', to: 'activities#new'
+  resources :activities, only: [:index, :show, :create, :new] do
+    resources :bookings, only: [:create, :new]
+    resources :bookings do
+      member do
+        patch :accept, :decline, :cancel
+      end
+    end
+  end
 end
+
+resource :
