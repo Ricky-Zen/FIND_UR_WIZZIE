7239b70d9c7d3736e51ad998a8e74bcd7e2709b1
Ricky Zenati <b00629577@essec.edu>
2021-02-16 10:36:50 +0100

    repaired broken booking model, activity controller, show view new of activity

--- db/migrate/20210215214221_remove_activity_to_bookings.rb
+++
@@ -1,5 +0,0 @@
-class RemoveActivityToBookings < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :bookings, :activity
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_170553) do
+ActiveRecord::Schema.define(version: 2021_02_15_171046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 2021_02_15_170553) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["theme_id"], name: "index_activities_on_theme_id"
     t.index ["user_id"], name: "index_activities_on_user_id"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "price"
+    t.date "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "themes", force: :cascade do |t|
@@ -50,4 +59,5 @@ ActiveRecord::Schema.define(version: 2021_02_15_170553) do
 
   add_foreign_key "activities", "themes"
   add_foreign_key "activities", "users"
+  add_foreign_key "bookings", "users"
 end
