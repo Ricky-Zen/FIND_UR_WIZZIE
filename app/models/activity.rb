b3594b6b3178ab8221b6797def3dcdef5e54af08
nasser madaran <nas.madaran@gmail.com>
2021-02-18 13:59:21 +0100

    searchbar final

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,28 @@
 class Activity < ApplicationRecord
   belongs_to :theme
   belongs_to :user
+
+
+  include PgSearch::Model
+  pg_search_scope :search_activity,
+    against: [ :name, :description ],
+    using: {
+      tsearch: { prefix: true }
+    }
+
+  pg_search_scope :search_city,
+    against: [ :city ],
+    using: {
+      tsearch: { prefix: true }
+    }
+
+  pg_search_scope :search_theme,
+    against: [],
+    associated_against: {
+      theme: [ :name ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
+
 end
