4c021937fea6bc88be9d42cd104ccd7b2ade8065
nasser madaran <nas.madaran@gmail.com>
2021-02-19 15:47:20 +0100

    maj post de contenu

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,13 +1,15 @@
 const init_nav_scroll = () => {
   const nav = document.querySelector('.transp-navbar')
 
-  window.addEventListener('scroll', (e) => {
-    if (window.scrollY > 500) {
-      nav.classList.remove('transp-navbar')
-    } else {
-      nav.classList.add('transp-navbar')
-    }
-  })
+  if (nav) {
+    window.addEventListener('scroll', (e) => {
+      if (window.scrollY > 500) {
+        nav.classList.remove('transp-navbar')
+      } else {
+        nav.classList.add('transp-navbar')
+      }
+    })
+  }
 }
 
 export { init_nav_scroll }
